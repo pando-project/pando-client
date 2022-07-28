98a07d055378f04a407c1f89820bdfa5d89c03ef
Kenny Issak <kennyissak@gmail.com>
2022-07-28 18:56:06 +0800

    add part of engine unit test

--- pkg/engine/engine_test.go
+++ pkg/engine/engine_test.go
@@ -3,18 +3,26 @@ package engine
 import (
 	"bytes"
 	"context"
+	"github.com/agiledragon/gomonkey/v2"
+	"github.com/filecoin-project/go-legs"
 	"github.com/filecoin-project/go-legs/dtsync"
 	"github.com/filecoin-project/go-legs/p2p/protocol/head"
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
 	dssync "github.com/ipfs/go-datastore/sync"
+	leveldb "github.com/ipfs/go-ds-leveldb"
+	"github.com/ipld/go-ipld-prime"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
 	"github.com/ipld/go-ipld-prime/node/basicnode"
 	"github.com/ipld/go-ipld-prime/storage/memstore"
 	"github.com/ipld/go-ipld-prime/traversal/selector"
 	selectorbuilder "github.com/ipld/go-ipld-prime/traversal/selector/builder"
+	"github.com/kenlabs/pando/pkg/types/schema"
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	. "github.com/smartystreets/goconvey/convey"
+	"pandoClient/cmd/server/command/config"
 	"sort"
 
 	"github.com/multiformats/go-multiaddr"
@@ -78,6 +86,14 @@ func TestMemEngineCreate(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, e.latestMeta, cid.Undef)
 	assert.Equal(t, e.pushList, []cid.Cid{})
+}
+func TestMemEngineShutdown(t *testing.T) {
+	e, err := New()
+	assert.NoError(t, err)
+	err = e.Start(context.Background())
+	assert.NoError(t, err)
+	err = e.Shutdown()
+	assert.NoError(t, err)
 }
 
 func TestEngineUpdateInfo(t *testing.T) {
@@ -235,4 +251,127 @@ func TestMetaInclusion(t *testing.T) {
 	res, err := e.pandoAPIClient.R().Get("/metadata/inclusion?cid=baguqeeqqw4xw7ahikwhdae2gz4wwiiaxre")
 	assert.NoError(t, err)
 	t.Log(string(res.Body()))
+}
+
+func TestGetPushedList(t *testing.T) {
+	Convey("Test Publish and GetPushedList", t, func() {
+		ds, err := leveldb.NewDatastore("", nil)
+		So(err, ShouldBeNil)
+
+		e, err := New(
+			WithDatastore(ds),
+			WithPublisherKind(DataTransferPublisher),
+		)
+		So(err, ShouldBeNil)
+
+		mt, err := schema.NewMetaWithBytesPayload([]byte("hello"), e.h.ID(), e.key)
+		So(err, ShouldBeNil)
+
+		ctx := context.Background()
+		err = e.Start(ctx)
+		So(err, ShouldBeNil)
+
+		c, err := e.Publish(ctx, *mt)
+		So(err, ShouldBeNil)
+		So(c, ShouldNotBeNil)
+
+		list, err := e.GetPushedList(ctx)
+		So(err, ShouldBeNil)
+		So(list, ShouldNotBeNil)
+		So(list[0], ShouldResemble, c)
+	})
+}
+
+func TestCatCid(t *testing.T) {
+	Convey("Test CatCid", t, func() {
+		ctx := context.Background()
+		e, err := New(
+			WithPublisherKind(DataTransferPublisher),
+		)
+		So(err, ShouldBeNil)
+
+		err = e.Start(ctx)
+		So(err, ShouldBeNil)
+
+		c, err := e.PublishBytesData(ctx, []byte("abc"))
+		So(err, ShouldBeNil)
+
+		data, err := e.CatCid(ctx, c)
+		So(err, ShouldBeNil)
+		So(data, ShouldResemble, []byte("abc"))
+	})
+}
+
+func TestRePublish(t *testing.T) {
+	Convey("Test RePublish", t, func() {
+		Convey("RePublishLatest", func() {
+			ctx := context.Background()
+			e, err := New(
+				WithPublisherKind(DataTransferPublisher),
+			)
+			So(err, ShouldBeNil)
+
+			err = e.Start(ctx)
+			So(err, ShouldBeNil)
+
+			c, err := e.PublishBytesData(ctx, []byte("abc"))
+			So(err, ShouldBeNil)
+
+			rc, err := e.RePublishLatest(ctx)
+			So(err, ShouldBeNil)
+			So(rc, ShouldResemble, c)
+		})
+		Convey("RePublishCid", func() {
+			ctx := context.Background()
+			e, err := New(
+				WithPublisherKind(DataTransferPublisher),
+			)
+			So(err, ShouldBeNil)
+
+			err = e.Start(ctx)
+			So(err, ShouldBeNil)
+
+			c1, err := e.PublishBytesData(ctx, []byte("abc"))
+			So(err, ShouldBeNil)
+			c2, err := e.PublishBytesData(ctx, []byte("xyz"))
+			So(err, ShouldBeNil)
+
+			Latest, err := e.RePublishLatest(ctx)
+			So(err, ShouldBeNil)
+			So(Latest, ShouldResemble, c2)
+
+			err = e.RePublishCid(ctx, c1)
+			So(err, ShouldBeNil)
+		})
+	})
+}
+
+func TestSyncFromPando(t *testing.T) {
+	Convey("Test Sync", t, func() {
+		pandoAddrInfo, err := config.GetPandoInfo()
+		So(err, ShouldBeNil)
+
+		info, err := pandoAddrInfo.AddrInfo()
+		So(err, ShouldBeNil)
+
+		ctx := context.Background()
+		e, err := New(
+			WithPublisherKind(DataTransferPublisher),
+			WithPandoAddrinfo(*info),
+		)
+		So(err, ShouldBeNil)
+
+		err = e.Start(ctx)
+		So(err, ShouldBeNil)
+
+		c, err := e.PublishBytesData(ctx, []byte("abc"))
+		So(err, ShouldBeNil)
+
+		patch := gomonkey.ApplyMethod(e.subscriber, "Sync", func(_ *legs.Subscriber, ctx context.Context, peerID peer.ID, nextCid cid.Cid, sel ipld.Node, peerAddr multiaddr.Multiaddr, opts ...legs.SyncOption) (cid.Cid, error) {
+			return cid.Cid{}, nil
+		})
+		defer patch.Reset()
+		_, err = e.Sync(ctx, c.String(), 0, "")
+		So(err, ShouldBeNil)
+	})
 }
